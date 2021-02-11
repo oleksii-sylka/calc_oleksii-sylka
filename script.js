c727c972b15bc9c315e51241f34acdbce1105db2
oleksii-sylka <oleksii.sylka@gmail.com>
2021-02-11 16:41:12 +0200

    modify function f_result

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var	second_num=0;
 var result = 0;
 
 
+
 let current_array = [];
 let history_array = [];
 
@@ -19,7 +20,6 @@ function get_value(elem_num){
 			document.getElementById("input_area").innerHTML = current_value;
 			//document.getElementById("history").innerHTML = history_value;
 }
-
 
 function del_symbol(){
 		if(current_array.length > 1){
@@ -34,81 +34,48 @@ function del_symbol(){
 		}
 }
 
-
-/*function f_operation(symbol_type){
-	if(current_value != 0 && symbol_type == '+'){
-		history_array.push(current_value);
-		history_array.push('+');
-		history_value = history_array.join('');
-		current_array = [];
-		document.getElementById("input_area").innerHTML = 0;
-		document.getElementById("history").innerHTML = history_value;
-		current_value = 0;
-	}
-	else if (current_value != 0 && symbol_type == '-') {
-		history_array.push(current_value);
-		history_array.push('-');
-		history_value = history_array.join('');
-		current_array = [];
-		document.getElementById("input_area").innerHTML = 0;
-		document.getElementById("history").innerHTML = history_value;
-		current_value = 0;
-	}
-}*/
-
 function f_operation(symbol_type){
+	history_array.push(current_value);
+	//history_array.pop();
 	switch(symbol_type){
 		case '+':
-			history_array.push(current_value);
 			history_array.push('+');
+			//alert('Ahtung!!!!')
 		break;
+		
 		case '-':
-			history_array.push(current_value);
 			history_array.push('-');
 		break;
+		
 		case '/':
-			history_array.push(current_value);
 			history_array.push('/');
 		break;
+		
 		case '*':
-			history_array.push(current_value);
 			history_array.push('*');
 		break;
-	}
+	//default: current_array.pop();
+
+	
+}
 	history_value = history_array.join('');
 	current_array = [];
 	document.getElementById("input_area").innerHTML = 0;
 	document.getElementById("history").innerHTML = history_value;
-	current_value = 0;
+	//current_value = 0;
 }
-
-
 
 
 function f_result(){
 	if(current_value != 0){	
 		history_array.push(current_value);
-		history_array.push('=');
+		//history_array.push('=');
 		history_value = history_array.join('');
 		current_array = [];
-	
-			for (j=0; j<history_array.length; j++){
-				if(history_array[j]=='+'){
-					//alert("найден +");
-					first_num=history_array[j-1];
-					second_num=history_array[j+1];
-					//result=Number(first_num)+Number(second_num);
-					result=Number(first_num)+Number(current_value);
-					
-					break;
-				}
-	
-			}
-	current_value=result;
-	
-	
+		result=eval(history_value);
+		//current_value=result;
 	}
-	document.getElementById("input_area").innerHTML = current_value;
+	document.getElementById("input_area").innerHTML = result;
 	document.getElementById("history").innerHTML = history_value;
 }
 
